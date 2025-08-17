9841113f99fc93143af8e668bd66cb3433f56d50
“IanMinor” <ianminor22@gmail.com>
2025-08-16 21:58:59 -0600

    feat: add changes

---
+++ frontend/ascendya/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import Dashboard from "./pages/Dashboard";
+import Quiz from "./pages/Quiz";
+import Navbar from "./components/layout/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
