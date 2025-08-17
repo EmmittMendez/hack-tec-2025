305f688aee8358befd215e54b51cb51acd127981
Emmitt Mendez <77180330+EmmittMendez@users.noreply.github.com>
2025-08-17 00:58:30 -0600

    Corregir main

--- frontend/ascendya/src/main.jsx
+++ frontend/ascendya/src/main.jsx
@@ -2,10 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import useStore from './store/userStore.js'
-
-// Inicializar autenticación al cargar la app
-useStore.getState().initAuth();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
